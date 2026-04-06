08a473044f9b7b72578347006302467a7e232cdb
Huu_Phuoc <phuoczmn@gmail.com>
2026-04-07 01:29:40 +0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,12 +1,16 @@
 from PIL import Image, ImageOps
 
-def predict_image(model, image: Image.Image, conf=0.35):
+def predict_image(model, image: Image.Image, conf=0.3):
     image = ImageOps.exif_transpose(image).convert("RGB")
+
+    # 🔥 resize nhỏ để nhẹ RAM
+    image = image.resize((320, 320))
 
     results = model.predict(
         source=image,
-        imgsz=320,          # 🔥 giảm RAM
+        imgsz=320,
         conf=conf,
+        device="cpu",
         verbose=False
     )
 
