ac14854bcc98add68367de4ff450ee967bcf182c
Huu_Phuoc <phuoczmn@gmail.com>
2026-04-07 01:22:39 +0700

    Refactor predict_image function in backend.py
    Refactor image prediction logic and remove model loading.

--- backend.py
+++ backend.py
@@ -1,22 +1,13 @@
-from ultralytics import YOLO
 from PIL import Image, ImageOps
-import torch
 
-# Load model 
-_model = None
-
-def load_model(path="best.pt"):
-    global _model
-    if _model is None:
-        try:
-            _model = YOLO(path)
-        except Exception as e:
-            raise RuntimeError(f"Không thể tải mô hình: {e}")
-    return _model
-
-def predict_image(image: Image.Image, conf=0.35, device=None):
-    model = load_model()
+def predict_image(model, image: Image.Image, conf=0.35):
     image = ImageOps.exif_transpose(image).convert("RGB")
-    device = 0 if torch.cuda.is_available() else "cpu"
-    results = model.predict(image, conf=conf, device=device, verbose=False)
+
+    results = model.predict(
+        source=image,
+        imgsz=320,          # 🔥 giảm RAM
+        conf=conf,
+        verbose=False
+    )
+
     return results
